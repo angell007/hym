c9697154119fc2a2e43bb90593750e57fe9f24e5
guillermojaramillo <guillermo.jaramillo@corvuslab.co>
2018-08-09 15:44:38 -0500

    Modulo Compras (Funcionalidades)

--- src/app/configuracion/compras/compras.component.ts
+++ src/app/configuracion/compras/compras.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, HostListener } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
 import { Globales } from '../../shared/globales/globales';
 
 @Component({
@@ -11,12 +12,120 @@ export class ComprasComponent implements OnInit {
 
   public fecha = new Date(); 
   public compras = [];
+  public Proveedores = [];
+  public Funcionarios = [];
+
+  //variables que hacen referencia a los campos del formulario editar   
+
+  public Identificacion : any[];
+  public Proveedor : any[];
+  public Valor : any[];
+  public TasaCambio : any[];
+  public Funcionario : any[];
+
+
+  @ViewChild('ModalCompra') ModalCompra:any;
+  @ViewChild('ModalVerCompra') ModalVerCompra:any;
+  @ViewChild('ModalEditarCompra') ModalEditarCompra:any;
+  @ViewChild('FormCompra') FormCompra:any;
+  @ViewChild('deleteSwal') deleteSwal:any;
+
+
   constructor(private http : HttpClient, private globales : Globales) { }
 
   ngOnInit() {
-    this.http.get(this.globales.ruta+'php/compras/lista.php').subscribe((data:any)=>{
-      this.compras = data;
+
+    this.ActualizarVista();
+    this.http.get(this.globales.ruta+'php/genericos/lista_generales.php',{ params: { modulo: 'Proveedor'}}).subscribe((data:any)=>{
+      this.Proveedores= data;
+    });
+    this.http.get(this.globales.ruta+'php/genericos/lista_generales.php',{ params: { modulo: 'Funcionario'}}).subscribe((data:any)=>{
+      this.Funcionarios= data;
     });
   }
+
+  @HostListener('document:keyup', ['$event']) handleKeyUp(event) {
+    if (event.keyCode === 27) {     
+      this.FormCompra.reset();
+      this.OcultarFormulario(this.ModalCompra);
+      this.OcultarFormulario(this.ModalVerCompra);
+      this.OcultarFormulario(this.ModalEditarCompra);
+    }
+  }
+
+  ActualizarVista()
+  {
+    this.http.get(this.globales.ruta+'php/compras/lista.php').subscribe((data:any)=>{
+      this.compras= data;
+    });
+  }
+
+  GuardarCompra(formulario: NgForm, modal:any){
+    let info = JSON.stringify(formulario.value);
+    let datos = new FormData();
+    datos.append("modulo",'Compra');
+    datos.append("datos",info);
+    this.OcultarFormulario(modal);
+    this.http.post(this.globales.ruta+'php/genericos/guardar_generico.php',datos).subscribe((data:any)=>{      
+    this.ActualizarVista();
+    formulario.reset();
+    });    
+  }
+
+
+  VerCompra(id, modal){
+    this.http.get(this.globales.ruta+'php/compras/detalle_compra.php',{
+      params:{id:id}
+    }).subscribe((data:any)=>{
+      this.Identificacion = id;
+      this.Proveedor = data.Proveedor;
+      this.Valor = data.Valor;
+      this.TasaCambio = data.Tasa_Cambio;
+      this.Funcionario = data.Funcionario;
+      modal.show();
+    });
+  }
+
+
+  EditarCompra(id, modal){
+    this.http.get(this.globales.ruta+'php/genericos/detalle.php',{
+      params:{modulo:'Compra', id:id}
+    }).subscribe((data:any)=>{
+  console.log(id);
+      this.Identificacion = id;
+      this.Proveedor = data.Id_Proveedor;
+      this.Valor = data.Valor;
+      this.TasaCambio = data.Tasa_Cambio;
+      this.Funcionario = data.Identificacion_Funcionario;
+      modal.show();
+    });
+  }
+
+
+  EliminarCompra(id){
+    console.log(id);  
+    let datos=new FormData();
+    datos.append("modulo", 'Compra');
+    datos.append ("id",id);
+    this.http.post(this.globales.ruta + 'php/genericos/eliminar_generico.php', datos ).subscribe((data:any)=>{
+      this.ActualizarVista();
+      this.deleteSwal.show();
+    })
+  }
+
+  OcultarFormulario(modal){
+    this.Identificacion = null;
+    this.Proveedor = null;
+    this.Valor = null;
+    this.TasaCambio = null;
+    this.Funcionario = null;
+    modal.hide();
+  }
+
+
+  Cerrar(modal){
+    this.OcultarFormulario(modal)
+  }
+
 
 }
