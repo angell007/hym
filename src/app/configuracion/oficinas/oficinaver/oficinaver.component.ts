fc90b23e32c739a48afe8e98c409148f525f9ba6
jairobricenoval <jairo.briceno@corvuslab.co>
2018-08-23 17:49:44 -0500

    oficinaver

--- src/app/configuracion/oficinas/oficinaver/oficinaver.component.ts
+++ src/app/configuracion/oficinas/oficinaver/oficinaver.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { Observable } from "rxjs";
+import { HttpClient } from '@angular/common/http';
+import { debounceTime, distinctUntilChanged, map, elementAt } from 'rxjs/operators';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Globales } from '../../../shared/globales/globales';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-oficinaver',
@@ -6,10 +13,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./oficinaver.component.scss']
 })
 export class OficinaverComponent implements OnInit {
+  id = this.route.snapshot.params["id"];
+  oficina: any;
+  Municipios: any;
+  Departamentos: any;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private http: HttpClient, private globales: Globales, private router: Router) { }
+
 
   ngOnInit() {
+
+    this.http.get(this.globales.ruta + 'php/genericos/lista_generales.php', { params: { modulo: 'Departamento' } }).subscribe((data: any) => {
+      this.Departamentos = data;
+    });
+
+    this.http.get(this.globales.ruta + '/php/oficinas/detalle_oficina.php', { params: { modulo: 'Oficina', id: this.id } }).subscribe((data: any) => {
+      this.Municipios_Departamento(data.Id_Departamento);
+      this.oficina = data;
+    });
+  }
+
+  Municipios_Departamento(Departamento) {
+    this.http.get(this.globales.ruta + 'php/genericos/municipios_departamento.php', { params: { id: Departamento } }).subscribe((data: any) => {
+      this.Municipios = data;
+    });
   }
 
 }
