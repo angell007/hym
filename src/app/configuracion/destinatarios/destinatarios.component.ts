02fd070b6dc1d66b3cb5cf03c37eb029db92dd61
jairobricenoval <jairo.briceno@corvuslab.co>
2018-08-28 16:32:38 -0500

    log out y destinatarios crear

--- src/app/configuracion/destinatarios/destinatarios.component.ts
+++ src/app/configuracion/destinatarios/destinatarios.component.ts
@@ -10,36 +10,36 @@ import { Subject } from 'rxjs/Subject';
 @Component({
   selector: 'app-destinatarios',
   templateUrl: './destinatarios.component.html',
-  styleUrls: ['./destinatarios.component.css']
+  styleUrls: ['./destinatarios.component.scss']
 })
 export class DestinatariosComponent implements OnInit {
 
-  public destinatarios : any[]=[];
-  public Paises : any[];
-  public Bancos : any[];
+  public destinatarios: any[] = [];
+  public Paises: any[];
+  public Bancos: any[];
 
   //variables de formulario
-  public Identificacion : any[];
-  public Nombre : any[];
-  public Cuentas : any[];
-  public IdBanco : any[];
-  public Banco : any[];
-  public IdPais : any[];
-  public Pais : any[];
-  public Detalle : any[];
-  public disabled : boolean = true;
+  public Identificacion: any[];
+  public Nombre: any[];
+  public Cuentas: any[];
+  public IdBanco: any[];
+  public Banco: any[];
+  public IdPais: any[];
+  public Pais: any[];
+  public Detalle: any[];
+  public disabled: boolean = true;
   public Lista_Cuentas = [];
-  public Detalle_Destinatario : any[] = [];
-  public Lista_Destinatarios:any=[{
-    Id_Pais:'',
+  public Detalle_Destinatario: any[] = [];
+  public Lista_Destinatarios: any = [{
+    Id_Pais: 2,
     Id_Banco: '',
-    Bancos : [],
-    Id_Tipo_Cuenta:'',
+    Bancos: [],
+    Id_Tipo_Cuenta: '',
     Numero_Cuenta: ''
   }]
 
-  public boolNombre:boolean = false;
-  public boolId:boolean = false;
+  public boolNombre: boolean = false;
+  public boolId: boolean = false;
   public cuentas: any[] = [];
   public datos: any[] = [];
   public Id_Destinatario: any;
@@ -49,47 +49,54 @@ export class DestinatariosComponent implements OnInit {
   bancoDefault: string = "";
   cuentasDefault: string = "";
 
-  @ViewChild('ModalVerDestinatario') ModalVerDestinatario:any;
-  @ViewChild('ModalEditarDestinatario') ModalEditarDestinatario:any;
-  @ViewChild('ModalDestinatario') ModalDestinatario:any;
-  @ViewChild('FormDestinatario') FormDestinatario:any;
-  @ViewChild('errorSwal') errorSwal:any;
-  @ViewChild('saveSwal') saveSwal:any;
-  @ViewChild('deleteSwal') deleteSwal:any;
-  @ViewChild('duplicateSwal') duplicateSwal:any;
+  @ViewChild('ModalVerDestinatario') ModalVerDestinatario: any;
+  @ViewChild('ModalEditarDestinatario') ModalEditarDestinatario: any;
+  @ViewChild('ModalDestinatario') ModalDestinatario: any;
+  @ViewChild('FormDestinatario') FormDestinatario: any;
+  @ViewChild('errorSwal') errorSwal: any;
+  @ViewChild('saveSwal') saveSwal: any;
+  @ViewChild('deleteSwal') deleteSwal: any;
+  @ViewChild('duplicateSwal') duplicateSwal: any;
   @ViewChild('confirmacionSwal') confirmacionSwal: any;
 
   dtOptions: DataTables.Settings = {};
   dtTrigger = new Subject();
 
+  public frame = false;
+  TipoDocumentoExtranjero = [];
+  url: string;
 
-  constructor(private http : HttpClient, private globales: Globales) { } 
+  constructor(private http: HttpClient, private globales: Globales) { }
 
   ngOnInit() {
     this.ActualizarVista();
   }
 
   @HostListener('document:keyup', ['$event']) handleKeyUp(event) {
-    if (event.keyCode === 27) {     
+    if (event.keyCode === 27) {
       this.OcultarFormularios();
     }
   }
 
-  ActualizarVista()
-  {
-    this.http.get(this.globales.ruta+'php/genericos/lista_generales.php',{ params: { modulo: 'Pais'}}).subscribe((data:any)=>{
-      this.Paises= data;
-      this.Bancos_Pais(2,0);
+  ActualizarVista() {
+    this.http.get(this.globales.ruta + 'php/genericos/lista_generales.php', { params: { modulo: 'Pais' } }).subscribe((data: any) => {
+      this.Paises = data;
+      this.Bancos_Pais(2, 0);
     });
-    this.http.get(this.globales.ruta+'php/genericos/lista_generales.php',{ params: { modulo: 'Banco'}}).subscribe((data:any)=>{
-      this.Bancos= data;
-    });
-    this.http.get(this.globales.ruta+'php/genericos/lista_generales.php',{ params: { modulo: 'Tipo_Cuenta'}}).subscribe((data:any)=>{
-      this.Cuentas= data;
+    this.http.get(this.globales.ruta + 'php/genericos/lista_generales.php', { params: { modulo: 'Banco' } }).subscribe((data: any) => {
+      this.Bancos = data;
     });
 
-    this.http.get(this.globales.ruta+'php/destinatarios/lista_destinatarios.php').subscribe((data:any)=>{
-      this.destinatarios= data;
+    this.http.get(this.globales.ruta + 'php/genericos/lista_generales.php', { params: { modulo: 'Tipo_Cuenta' } }).subscribe((data: any) => {
+      this.Cuentas = data;
+    });
+
+    this.http.get(this.globales.ruta + 'php/genericos/lista_generales.php', { params: { modulo: 'Tipo_Documento_Extranjero' } }).subscribe((data: any) => {
+      this.TipoDocumentoExtranjero = data;
+    });
+
+    this.http.get(this.globales.ruta + 'php/destinatarios/lista_destinatarios.php').subscribe((data: any) => {
+      this.destinatarios = data;
       this.dtTrigger.next();
     });
 
@@ -98,7 +105,7 @@ export class DestinatariosComponent implements OnInit {
       pageLength: 10,
       dom: 'Bfrtip',
       responsive: true,
-      /* below is the relevant part, e.g. translated to spanish */ 
+      /* below is the relevant part, e.g. translated to spanish */
       language: {
         processing: "Procesando...",
         search: "Buscar:",
@@ -121,25 +128,24 @@ export class DestinatariosComponent implements OnInit {
           sortDescending: ": Activar para ordenar la tabla en orden descendente"
         }
       }
-    }; 
+    };
   }
 
-  Bancos_Pais(Pais,i){
-    this.http.get(this.globales.ruta+'php/genericos/bancos_pais.php',{ params: { id: Pais}}).subscribe((data:any)=>{
-      this.Lista_Destinatarios[i].Bancos = data;     
+  Bancos_Pais(Pais, i) {
+    this.http.get(this.globales.ruta + 'php/genericos/bancos_pais.php', { params: { id: Pais } }).subscribe((data: any) => {
+      this.Lista_Destinatarios[i].Bancos = data;
     });
   }
 
-  agregarDinamico(i){
-    var pos = parseInt(i)+1;
-      if(this.Lista_Destinatarios[pos] == undefined){
-        this.AgregarFila();
-        this.Bancos_Pais(2,pos);
-      }
+  agregarDinamico(i) {
+    var pos = parseInt(i) + 1;
+    if (this.Lista_Destinatarios[pos] == undefined) {
+      this.AgregarFila();
+      this.Bancos_Pais(2, pos);
+    }
   }
 
-  OcultarFormularios()
-  {
+  OcultarFormularios() {
     this.InicializarBool();
     this.OcultarFormulario(this.ModalDestinatario);
     this.OcultarFormulario(this.ModalVerDestinatario);
@@ -147,9 +153,9 @@ export class DestinatariosComponent implements OnInit {
   }
 
   BuscarIdentificacion(id) {
-    this.http.get(this.globales.ruta+'php/destinatarios/detalle_destinatario.php',{
-      params:{id:id}
-    }).subscribe((data:any)=>{
+    this.http.get(this.globales.ruta + 'php/destinatarios/detalle_destinatario.php', {
+      params: { id: id }
+    }).subscribe((data: any) => {
       this.datos = data.Cuentas;
       if (this.datos.length > 0) {
         this.confirmacionSwal.title = "Error en la Identificacion";
@@ -157,16 +163,54 @@ export class DestinatariosComponent implements OnInit {
         this.confirmacionSwal.type = "error";
         this.confirmacionSwal.show();
         this.Id_Destinatario = "";
-        
+      } else {
+        var tipoDoc = ((document.getElementById("Tipo_Documento") as HTMLInputElement).value);
+        this.buscarCne(id,tipoDoc);
       }
     });
-    
+
   }
 
-  InicializarBool()
-  {
+  codigoBanco(seleccion, posicion, texto) {
+
+    var pais = ((document.getElementById("Id_Pais" + posicion) as HTMLInputElement).value);
+
+    if (pais == "2") {
+      switch (texto) {
+        case "check": {
+          var buscarBanco = this.Bancos.findIndex(x => x.Id_Banco === seleccion)
+          this.Lista_Destinatarios[posicion].Numero_Cuenta = this.Bancos[buscarBanco].Identificador;
+          break;
+        }
+        case "input": {
+          if(seleccion.length == 4){
+            var buscarBanco = this.Bancos.findIndex(x => x.Identificador === seleccion)
+            if(buscarBanco > -1){
+              this.Lista_Destinatarios[posicion].Id_Banco = this.Bancos[buscarBanco].Id_Banco;
+            }else{
+              this.Lista_Destinatarios[posicion].Id_Banco = '';
+            }
+          }else{
+            if(seleccion.length < 4 && seleccion.length > 0){
+              this.Lista_Destinatarios[posicion].Id_Banco = '';
+            }
+          }
+          break;
+        }
+      }
+    }
+
+  }
+
+  InicializarBool() {
     this.boolNombre = false;
     this.boolId = false;
+  }
+
+  buscarCne(id, tipo) {
+    
+    this.url = "http://cne.gov.ve/web/registro_electoral/ce.php?nacionalidad=" + tipo + "&cedula=" + id;
+    this.frame = true;
   }
 
   /**
@@ -176,140 +220,140 @@ export class DestinatariosComponent implements OnInit {
    * @param {NgForm} formulario
    * @memberof DestinatariosComponent
    */
-  GuardarDestinatario(formulario: NgForm, modal:any){
-    //console.log(formulario.value);
-    console.log(this.Lista_Destinatarios);
+  GuardarDestinatario(formulario: NgForm, modal: any) {
     let info = JSON.stringify(formulario.value);
     let destinatario = JSON.stringify(this.Lista_Destinatarios);
     let datos = new FormData();
     this.OcultarFormulario(modal);
     //datos.append("modulo",'Destinatario');
-    datos.append("datos",info);
-    datos.append("destinatario",destinatario);
-    this.http.post(this.globales.ruta + 'php/destinatarios/guardar_destinatario.php',datos)
-    .catch(error => { 
-      console.error('An error occurred:', error.error);
-      this.errorSwal.show();
-      return this.handleError(error);
-    })
-    .subscribe((data:any)=>{
-      this.Lista_Destinatarios = [{
-        Id_Pais:'',
-        Id_Banco: '',
-        Bancos : [],
-        Id_Tipo_Cuenta:'',
-        Numero_Cuenta: ''
-      }];
-      localStorage.removeItem("Lista_Inicial");
-      formulario.reset();
-      this.ActualizarVista();
-      this.saveSwal.show();
-     });
-    }
-
-
-
-    GuardarDestinatarioEditar(formulario: NgForm, modal:any){
-      let info = JSON.stringify(formulario.value);
-      let datos = new FormData();
-      this.OcultarFormulario(modal);
-      datos.append("modulo",'Destinatario');
-      datos.append("datos",info);
-      this.http.post(this.globales.ruta+'php/genericos/guardar_generico.php',datos)
-      .catch(error => { 
+    datos.append("datos", info);
+    datos.append("destinatario", destinatario);
+    this.http.post(this.globales.ruta + 'php/destinatarios/guardar_destinatario.php', datos)
+      .catch(error => {
         console.error('An error occurred:', error.error);
         this.errorSwal.show();
         return this.handleError(error);
       })
-      .subscribe((data:any)=>{ 
+      .subscribe((data: any) => {
         this.Lista_Destinatarios = [{
-          Id_Pais:'',
+          Id_Pais: '',
           Id_Banco: '',
-          Bancos : [],
-          Id_Tipo_Cuenta:'',
+          Bancos: [],
+          Id_Tipo_Cuenta: '',
           Numero_Cuenta: ''
         }];
-        
+        localStorage.removeItem("Lista_Inicial");
+        formulario.reset();
+        this.ActualizarVista();
+        this.saveSwal.show();
+      });
+  }
+
+  GuardarDestinatarioEditar(formulario: NgForm, modal: any) {
+    let info = JSON.stringify(formulario.value);
+    let datos = new FormData();
+    this.OcultarFormulario(modal);
+    datos.append("modulo", 'Destinatario');
+    datos.append("datos", info);
+    this.http.post(this.globales.ruta + 'php/genericos/guardar_generico.php', datos)
+      .catch(error => {
+        console.error('An error occurred:', error.error);
+        this.errorSwal.show();
+        return this.handleError(error);
+      })
+      .subscribe((data: any) => {
+        this.Lista_Destinatarios = [{
+          Id_Pais: '',
+          Id_Banco: '',
+          Bancos: [],
+          Id_Tipo_Cuenta: '',
+          Numero_Cuenta: ''
+        }];
+
         this.ActualizarVista();
         this.InicializarBool();
         this.saveSwal.show();
       });
-      
-    }
+
+  }
 
   handleError(error: Response) {
     return Observable.throw(error);
   }
 
-  VerDestinatario(id, modal){
-    this.http.get(this.globales.ruta+'php/destinatarios/detalle_destinatario.php',{
-      params:{id:id}
-    }).subscribe((data:any)=>{
+  VerDestinatario(id, modal) {
+    this.http.get(this.globales.ruta + 'php/destinatarios/detalle_destinatario.php', {
+      params: { id: id }
+    }).subscribe((data: any) => {
       this.Detalle_Destinatario = data;
       this.cuentas = data.Cuentas;
       this.Identificacion = id;
       modal.show();
     });
-   
+
   }
-  
-  EditarDestinatario(id){
-    this.InicializarBool(); 
-    this.http.get(this.globales.ruta+'php/destinatarios/editar_destinatario.php',{
-      params:{id:id}
-    }).subscribe((data:any)=>{
-      console.log(data);      
+
+  EditarDestinatario(id) {
+    this.InicializarBool();
+    this.http.get(this.globales.ruta + 'php/destinatarios/editar_destinatario.php', {
+      params: { id: id }
+    }).subscribe((data: any) => {
+      console.log(data);
       this.Detalle_Destinatario = data;
       console.log(this.Detalle_Destinatario);
-      
+
       this.Lista_Destinatarios = data;
-       //this.cuentas = data.Cuentas;
+      //this.cuentas = data.Cuentas;
       this.Identificacion = id;
       this.ModalEditarDestinatario.show();
     });
   }
 
-  EliminarDestinatario(id){
+  EliminarDestinatario(id) {
     let datos = new FormData();
     datos.append("modulo", 'Destinatario');
-    datos.append("id", id); 
-    this.http.post(this.globales.ruta + 'php/genericos/anular_generico.php', datos ).subscribe((data: any) => {
+    datos.append("id", id);
+    this.http.post(this.globales.ruta + 'php/genericos/anular_generico.php', datos).subscribe((data: any) => {
       this.deleteSwal.show();
       this.ActualizarVista();
     });
   }
 
-  OcultarFormulario(modal)
-  {
+  OcultarFormulario(modal) {
     this.Identificacion = null;
     this.Nombre = null;
     this.Cuentas = null;
-    this.IdBanco = null; 
+    this.IdBanco = null;
     this.IdPais = null;
     this.Detalle = null;
     modal.hide();
   }
 
 
-  AgregarFila() { 
-    
-  this.Lista_Destinatarios.push({
-  Pais:'2',
-  Banco: "",
-  Cuenta:"",
-  Numero_Cuenta: ''
-})
-  }
-  EliminarFila(i){ 
-    
-    this.Lista_Cuentas.splice(i,1);
+  AgregarFila() {
+
+    this.Lista_Destinatarios.push({
+      Id_Pais: 2,
+      Id_Banco: '',
+      Bancos: [],
+      Id_Tipo_Cuenta: '',
+      Numero_Cuenta: ''
+    })
+
+    console.log(this.Lista_Destinatarios)
   }
 
-  Cerrar(modal){
+
+  EliminarFila(i) {
+
+    this.Lista_Cuentas.splice(i, 1);
+  }
+
+  Cerrar(modal) {
     this.OcultarFormulario(modal)
   }
 
-  habilitarboton(){
+  habilitarboton() {
     this.disabled = false;
   }
 
