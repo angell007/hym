c83787ec0534ad2abab1e677bd31d721c9ce3c09
jairo <jairo.briceno@corvuslab.co>
2018-10-09 15:22:48 -0500

    arreglos visuales y arreglos de guardado

--- src/app/configuracion/cuentasbancarias/cuentasbancariasver/cuentasbancariasver.component.ts
+++ src/app/configuracion/cuentasbancarias/cuentasbancariasver/cuentasbancariasver.component.ts
@@ -1,10 +1,9 @@
-import { Component, OnInit, ViewChild} from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Globales } from '../../../shared/globales/globales';
 import { NgForm } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Subject } from 'rxjs';
-
 
 @Component({
   selector: 'app-cuentasbancariasver',
@@ -13,33 +12,33 @@ import { Subject } from 'rxjs';
 })
 export class CuentasbancariasverComponent implements OnInit {
 
-
   @ViewChild('confirmacionSwal') confirmacionSwal: any;
   @ViewChild('ModalVerRecibo') ModalVerRecibo: any;
-  @ViewChild('ModalAjusteEditar') ModalAjusteEditar: any;   
-  
-  Movimientos=[];
-  id = this.route.snapshot.params["id"];
+  @ViewChild('ModalAjusteEditar') ModalAjusteEditar: any;
+
+  Movimientos = [];
+  public id = this.route.snapshot.params["id"];
   dtOptions: DataTables.Settings = {};
   dtTrigger = new Subject();
   SaldoActual: any;
   DatosBanco = [];
   EncabezadoRecibo = [{
-    
+
   }];
-  DestinatarioRecibo= [];
-  DevolucionesRecibo= [];
+  DestinatarioRecibo = [];
+  DevolucionesRecibo = [];
   filaRecibo = false;
 
-  constructor(private route: ActivatedRoute,private http : HttpClient, private globales: Globales) { }
+  constructor(private route: ActivatedRoute, private http: HttpClient, private globales: Globales) { }
 
-  ngOnInit() {
+  ngOnInit() {    
+    this.id = this.route.snapshot.params["id"];
     this.ActualizarVista();
     this.calcularSaldoActual();
     this.datosCuentaBancaria();
   }
 
-  datosCuentaBancaria(){
+  datosCuentaBancaria() {
     this.http.get(this.globales.ruta + 'php/bancos/detalle_banco.php', {
       params: { id: this.id }
     }).subscribe((data: any) => {
@@ -47,25 +46,25 @@ export class CuentasbancariasverComponent implements OnInit {
     });
   }
 
-  ActualizarVista(){
-    this.id = this.route.snapshot.params["id"];
+  ActualizarVista() {
     this.http.get(this.globales.ruta + 'php/movimientos/movimiento_cuenta_bancaria.php', {
-      params: { id:this.id }
+      params: { id: this.id }
     }).subscribe((data: any) => {
       this.Movimientos = data.lista;
     });
-
-    
   }
 
   GuardarMovimiento(formulario: NgForm, modal) {
     let info = JSON.stringify(formulario.value);
     let datos = new FormData();
+    var newId = this.id;
     datos.append("modulo", 'Movimiento_Cuenta_Bancaria');
     datos.append("datos", info);
+    datos.append("Id_Cuenta_Bancaria", this.id);
     this.http.post(this.globales.ruta + 'php/bancos/guardar_ajuste.php', datos)
       .subscribe((data: any) => {
-        formulario.reset();
+       formulario.reset();
+        (document.getElementById("Id_Cuenta_Bancaria") as HTMLInputElement).value=newId;
         modal.hide();
         this.ActualizarVista();
         this.confirmacionSwal.title = "Movimiento Realizado"
@@ -74,22 +73,21 @@ export class CuentasbancariasverComponent implements OnInit {
         this.confirmacionSwal.show();
         this.calcularSaldoActual();
       });
-
   }
-  calcularSaldoActual(){
+
+  calcularSaldoActual() {
     this.http.get(this.globales.ruta + 'php/movimientos/movimiento_cuenta_bancaria.php', {
       params: { id: this.id }
     }).subscribe((data: any) => {
-      if(data.total > 0){
+      if (data.total > 0) {
         this.SaldoActual = data.total;
-      }else{
+      } else {
         this.SaldoActual = 0;
       }
-
     });
   }
 
-  verRecibo(valor){
+  verRecibo(valor) {
     this.http.get(this.globales.ruta + 'php/transferencias/ver_recibo.php', {
       params: { id: valor }
     }).subscribe((data: any) => {
@@ -97,17 +95,16 @@ export class CuentasbancariasverComponent implements OnInit {
       this.DestinatarioRecibo = data.destinatario;
       this.DevolucionesRecibo = data.devoluciones;
 
-      if(this.DevolucionesRecibo.length > 0){
+      if (this.DevolucionesRecibo.length > 0) {
         this.filaRecibo = true;
-      }else{
+      } else {
         this.filaRecibo = false;
       }
-
-      this.ModalVerRecibo.show();      
-    });    
+      this.ModalVerRecibo.show();
+    });
   }
 
-  borrarAjuste(id){
+  borrarAjuste(id) {
     let datos = new FormData();
     datos.append("modulo", 'Movimiento_Cuenta_Bancaria');
     datos.append("id", id);
@@ -122,17 +119,16 @@ export class CuentasbancariasverComponent implements OnInit {
       });
   }
 
-  idItem:any;
+  idItem: any;
   ajusteBancario = [];
-  editarAjuste(id , modal){
+  editarAjuste(id, modal) {
     this.idItem = id;
     this.http.get(this.globales.ruta + 'php/genericos/detalle.php', {
-      params: { id: id , modulo:"Movimiento_Cuenta_Bancaria"}
+      params: { id: id, modulo: "Movimiento_Cuenta_Bancaria" }
     }).subscribe((data: any) => {
       this.ajusteBancario = data;
     });
-
     modal.show();
-   }
-  
+  }
+
 }
